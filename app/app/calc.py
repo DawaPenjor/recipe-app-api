ac240bf3882633ce95595722e4102da90f15e162
Dawa Penjor <thewapenjor55@gmail.com>
2024-04-08 23:41:49 +0800

    updated database connection

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+
+'''
+Calculator functions
+'''
+
+def addition(x, y):
+    """adds two numbers"""
+    return x+y
+
+def subtract(x, y):
+    """subtract two numbers"""
+    return x-y
---
+++ app/app/tests.py
@@ -0,0 +1,20 @@
+"""
+sample test
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTest(SimpleTestCase):
+    def test_add_numbers(self):
+        """adding two numbers together"""
+        res = calc.addition(3,5)
+
+        self.assertEqual(res, 8)
+
+    def test_subtract_number(self):
+        """Subtract two nhumbers"""
+        res = calc.subtract(15, 10)
+
+        self.assertEqual(res, 5)    
+    
